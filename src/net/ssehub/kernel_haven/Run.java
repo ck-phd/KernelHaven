df684dc0dcb2113040866f1232ecbbe96cbef443
Christian Kroeher <kroeher@sse.uni-hildesheim.de>
2021-01-19 15:45:46 +0100

    Remove redundant import

--- src/net/ssehub/kernel_haven/Run.java
+++ src/net/ssehub/kernel_haven/Run.java
@@ -22,7 +22,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
